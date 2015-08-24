5bfc3d24321f6dd84d739d9e027a6b599f4fa479
Aniket Panse <aniket@getpostman.com>
2015-08-24 14:35:36 +0530

    tabs => spaces

--- convert.js
+++ convert.js
@@ -3,318 +3,317 @@ var uuid = require('node-uuid');
 var jsface = require('jsface');
 
 function ConvertResult(status, message) {
-	this.status = status;
-	this.message = message;
+    this.status = status;
+    this.message = message;
 }
 
 var Swagger2Postman = jsface.Class({
-	constructor: function() {
-	    this.collectionJson = {
-			"id": "",
-			"name": "",
-			"description": "",
-			"order": [
-			],
-			"folders": [],
-			"timestamp": 1413302258635,
-			"synced": false,
-			"requests": [
-			]
-		};
+    constructor: function () {
+        this.collectionJson = {
+            "id": "",
+            "name": "",
+            "description": "",
+            "order": [],
+            "folders": [],
+            "timestamp": 1413302258635,
+            "synced": false,
+            "requests": []
+        };
         this.basePath = "";
         this.collectionId = "";
         this.folders = {};
         this.baseParams = {};
-        this.logger = function(){};
-  	},
+        this.logger = function () {
+        };
+    },
 
-  	setLogger: function(func) {
-  		this.logger = func;
-  	},
+    setLogger: function (func) {
+        this.logger = func;
+    },
 
-  	main: function(Swagger2Postman) {
+    main: function (Swagger2Postman) {
 
-  	},
+    },
 
-	validate: function(json) {
-		if(!json.hasOwnProperty("swagger") || json.swagger!=="2.0") {
-			return new ConvertResult("failed", "Must contain a swagger field (2.0)");
-		}
+    validate: function (json) {
+        if (!json.hasOwnProperty("swagger") || json.swagger !== "2.0") {
+            return new ConvertResult("failed", "Must contain a swagger field (2.0)");
+        }
 
-		if(!json.hasOwnProperty("info")) {
-			return new ConvertResult("failed", "Must contain an info object");	
-		}
-		else { 
-			var info = json.info;
-			if(!info.title) {
-				return new ConvertResult("failed", "Must contain info.title");
-			}
-		}
+        if (!json.hasOwnProperty("info")) {
+            return new ConvertResult("failed", "Must contain an info object");
+        }
+        else {
+            var info = json.info;
+            if (!info.title) {
+                return new ConvertResult("failed", "Must contain info.title");
+            }
+        }
 
-		return new ConvertResult("passed","");
-	},
+        return new ConvertResult("passed", "");
+    },
 
-	setBasePath: function(json) {
-		this.basePath = "";
-		if(json.host) {
-			this.basePath = json.host;
-		}
-		if(json.basePath) {
-			this.basePath += json.basePath;
-		}
+    setBasePath: function (json) {
+        this.basePath = "";
+        if (json.host) {
+            this.basePath = json.host;
+        }
+        if (json.basePath) {
+            this.basePath += json.basePath;
+        }
 
-		if(json.schemas && json.schemas.indexOf("https")!=-1) {
-			this.basePath = "https://" + this.basePath;
-		}
-		else {
-			this.basePath = "http://" + this.basePath;
-		}
-	},
+        if (json.schemas && json.schemas.indexOf("https") != -1) {
+            this.basePath = "https://" + this.basePath;
+        }
+        else {
+            this.basePath = "http://" + this.basePath;
+        }
+    },
 
-	getFolderNameForPath: function(pathUrl) {
-		var segments = pathUrl.split("/");
-		var numSegments = segments.length;
-		var folderName = null;
-		this.logger("Getting folder name for path: " + pathUrl);
-		this.logger("Segments: " + JSON.stringify(segments));
-		if(numSegments>1) {
-			folderName = segments[1];
+    getFolderNameForPath: function (pathUrl) {
+        var segments = pathUrl.split("/");
+        var numSegments = segments.length;
+        var folderName = null;
+        this.logger("Getting folder name for path: " + pathUrl);
+        this.logger("Segments: " + JSON.stringify(segments));
+        if (numSegments > 1) {
+            folderName = segments[1];
 
-			//create a folder for this path url
-			if(!this.folders[folderName]) {
-				this.folders[folderName] = this.createNewFolder(folderName);
-			}
-			this.logger("For path " + pathUrl + ", returning folderName " + this.folders[folderName].name);
-			return this.folders[folderName].name;
-		}
-		else {
-			this.logger("Error - path MUST begin with /");
-			return null;
-		}
-	},
+            //create a folder for this path url
+            if (!this.folders[folderName]) {
+                this.folders[folderName] = this.createNewFolder(folderName);
+            }
+            this.logger("For path " + pathUrl + ", returning folderName " + this.folders[folderName].name);
+            return this.folders[folderName].name;
+        }
+        else {
+            this.logger("Error - path MUST begin with /");
+            return null;
+        }
+    },
 
-	createNewFolder: function(name) {
-		var newFolder = {
-			"id": uuid.v4(),
-			"name": name,
-			"description": "Folder for " + name,
-			"order": [],
-			"collection_name": this.collectionJson.name,
-			"collection_id": this.collectionId,
-			"collection": this.collectionId,
-		};
-		this.logger("Created folder " + newFolder.name);
-		return newFolder;
-	},
+    createNewFolder: function (name) {
+        var newFolder = {
+            "id": uuid.v4(),
+            "name": name,
+            "description": "Folder for " + name,
+            "order": [],
+            "collection_name": this.collectionJson.name,
+            "collection_id": this.collectionId,
+            "collection": this.collectionId,
+        };
+        this.logger("Created folder " + newFolder.name);
+        return newFolder;
+    },
 
-	handleInfo: function(json) {
-		this.collectionJson.name = json.info.title;
-		this.collectionJson.description = json.info.description;
-	},
+    handleInfo: function (json) {
+        this.collectionJson.name = json.info.title;
+        this.collectionJson.description = json.info.description;
+    },
 
-	getParamsForPathItem: function(oldParams, newParams) {
-		if(!oldParams) oldParams = [];
-		if(!newParams) newParams = [];
+    getParamsForPathItem: function (oldParams, newParams) {
+        if (!oldParams) oldParams = [];
+        if (!newParams) newParams = [];
 
-		var root = this;
-		var retVal = {};
-		var numOldParams = oldParams.length;
-		var numNewParams = newParams.length;
+        var root = this;
+        var retVal = {};
+        var numOldParams = oldParams.length;
+        var numNewParams = newParams.length;
 
-		for(var i=0;i<numOldParams;i++) {
-			if(oldParams[i]["$ref"]) {
-				//this is a ref
-				if(oldParams[i]["$ref"].indexOf("#/parameters") == 0) {
-					var parts = oldParams[i]["$ref"].split("/");
-					var lastPart = parts[parts.length-1];
-					var getBaseParam = this.baseParams[lastPart];
-					retVal[lastPart] = getBaseParam;
-				}
-			}
-			else {	
-				retVal[oldParams[i].name] = oldParams[i];
-			}
-		}
+        for (var i = 0; i < numOldParams; i++) {
+            if (oldParams[i]["$ref"]) {
+                //this is a ref
+                if (oldParams[i]["$ref"].indexOf("#/parameters") == 0) {
+                    var parts = oldParams[i]["$ref"].split("/");
+                    var lastPart = parts[parts.length - 1];
+                    var getBaseParam = this.baseParams[lastPart];
+                    retVal[lastPart] = getBaseParam;
+                }
+            }
+            else {
+                retVal[oldParams[i].name] = oldParams[i];
+            }
+        }
 
-		for(var i=0;i<numNewParams;i++) {
-			if(newParams[i]["$ref"]) {
-				//this is a ref
-				if(newParams[i]["$ref"].indexOf("#/parameters") == 0) {
-					var parts = newParams[i]["$ref"].split("/");
-					var lastPart = parts[parts.length-1];
-					var getBaseParam = this.baseParams[lastPart];
-					retVal[lastPart] = getBaseParam;
-				}
-			}
-			else {	
-				retVal[newParams[i].name] = newParams[i];
-			}
-		}
+        for (var i = 0; i < numNewParams; i++) {
+            if (newParams[i]["$ref"]) {
+                //this is a ref
+                if (newParams[i]["$ref"].indexOf("#/parameters") == 0) {
+                    var parts = newParams[i]["$ref"].split("/");
+                    var lastPart = parts[parts.length - 1];
+                    var getBaseParam = this.baseParams[lastPart];
+                    retVal[lastPart] = getBaseParam;
+                }
+            }
+            else {
+                retVal[newParams[i].name] = newParams[i];
+            }
+        }
 
-		return retVal;
-	},
+        return retVal;
+    },
 
-	addOperationToFolder: function(path, method, operation, folderName, params) {
-		var root = this;
-		var request = {
-				"id": uuid.v4(),
-				"headers": "",
-				"url": "",
-				"pathVariables": {},
-				"preRequestScript": "",
-				"method": "GET",
-				"data": [
-					{
-						"key": "size",
-						"value": "original",
-						"type": "text",
-						"enabled": true
-					}
-				],
-				"dataMode": "params",
-				"description": "",
-				"descriptionFormat": "html",
-				"time": "",
-				"version": 2,
-				"responses": [],
-				"tests": "",
-				"collectionId": root.collectionId,
-				"synced": false
-			};
+    addOperationToFolder: function (path, method, operation, folderName, params) {
+        var root = this;
+        var request = {
+            "id": uuid.v4(),
+            "headers": "",
+            "url": "",
+            "pathVariables": {},
+            "preRequestScript": "",
+            "method": "GET",
+            "data": [
+                {
+                    "key": "size",
+                    "value": "original",
+                    "type": "text",
+                    "enabled": true
+                }
+            ],
+            "dataMode": "params",
+            "description": "",
+            "descriptionFormat": "html",
+            "time": "",
+            "version": 2,
+            "responses": [],
+            "tests": "",
+            "collectionId": root.collectionId,
+            "synced": false
+        };
 
-		request.description = operation.description;
+        request.description = operation.description;
 
-		var thisParams = this.getParamsForPathItem(params, operation.parameters);
-		request.url = this.basePath + path;
-		request.method = method;
-		request.name = operation.summary;
-		request.time = (new Date()).getTime();
+        var thisParams = this.getParamsForPathItem(params, operation.parameters);
+        request.url = this.basePath + path;
+        request.method = method;
+        request.name = operation.summary;
+        request.time = (new Date()).getTime();
 
-		var hasQueryParams = false;
+        var hasQueryParams = false;
 
-		//set data and headers
-		for(param in thisParams) {
-			if(thisParams.hasOwnProperty(param)) {
-				this.logger("Processing param: " + JSON.stringify(param));
-				if(thisParams[param].in === "query") {
-					if(!hasQueryParams) {
-						hasQueryParams = true;
-						request.url += "?";
-					}
-					request.url += thisParams[param].name + "={{" + thisParams[param].name + "}}&";
-				}
+        //set data and headers
+        for (param in thisParams) {
+            if (thisParams.hasOwnProperty(param)) {
+                this.logger("Processing param: " + JSON.stringify(param));
+                if (thisParams[param].in === "query") {
+                    if (!hasQueryParams) {
+                        hasQueryParams = true;
+                        request.url += "?";
+                    }
+                    request.url += thisParams[param].name + "={{" + thisParams[param].name + "}}&";
+                }
 
-				else if(thisParams[param].in==="header") {
-					request.headers += thisParams[param].name + ": {{" + thisParams[param].name + "}}\n";
-				}
+                else if (thisParams[param].in === "header") {
+                    request.headers += thisParams[param].name + ": {{" + thisParams[param].name + "}}\n";
+                }
 
-				else if(thisParams[param].in==="path") {
-					//meh
-				}
+                else if (thisParams[param].in === "path") {
+                    //meh
+                }
 
-				else if(thisParams[param].in==="formData") {
-					request.dataMode = "params";
-					request.data.push({
-						"key": thisParams[param].name,
-						"value": "{{" + thisParams[param].name + "}}",
-						"type": "text",
-						"enabled": true
-					});
-				}
-			}
-		}
+                else if (thisParams[param].in === "formData") {
+                    request.dataMode = "params";
+                    request.data.push({
+                        "key": thisParams[param].name,
+                        "value": "{{" + thisParams[param].name + "}}",
+                        "type": "text",
+                        "enabled": true
+                    });
+                }
+            }
+        }
 
-		this.collectionJson.requests.push(request);
-		this.folders[folderName].order.push(request.id);
-	},
+        this.collectionJson.requests.push(request);
+        this.folders[folderName].order.push(request.id);
+    },
 
-	addPathItemToFolder: function(path, pathItem, folderName) {
-		if(pathItem["$ref"]) {
-			this.logger("Error - cannot handle $ref attributes");
-			return;
-		}
+    addPathItemToFolder: function (path, pathItem, folderName) {
+        if (pathItem["$ref"]) {
+            this.logger("Error - cannot handle $ref attributes");
+            return;
+        }
 
-		var paramsForPathItem = this.getParamsForPathItem(this.baseParams, pathItem.parameters);
+        var paramsForPathItem = this.getParamsForPathItem(this.baseParams, pathItem.parameters);
 
-		//replace path variables {petId} with {{..}}
-		if(path) {
-			path = path.replace("{","{{").replace("}","}}");
-		}
+        //replace path variables {petId} with {{..}}
+        if (path) {
+            path = path.replace("{", "{{").replace("}", "}}");
+        }
 
-		if(pathItem.get) this.addOperationToFolder(path, "GET", pathItem.get, folderName, paramsForPathItem);
-		if(pathItem.put) this.addOperationToFolder(path, "PUT", pathItem.put, folderName, paramsForPathItem);
-		if(pathItem.post) this.addOperationToFolder(path, "POST", pathItem.post, folderName, paramsForPathItem);
-		if(pathItem.delete) this.addOperationToFolder(path, "DELETE", pathItem.delete, folderName, paramsForPathItem);
-		if(pathItem.options) this.addOperationToFolder(path, "OPTIONS", pathItem.options, folderName, paramsForPathItem);
-		if(pathItem.head) this.addOperationToFolder(path, "HEAD", pathItem.head, folderName, paramsForPathItem);
-		if(pathItem.path) this.addOperationToFolder(path, "PATH", pathItem.path, folderName, paramsForPathItem);
-	},
+        if (pathItem.get) this.addOperationToFolder(path, "GET", pathItem.get, folderName, paramsForPathItem);
+        if (pathItem.put) this.addOperationToFolder(path, "PUT", pathItem.put, folderName, paramsForPathItem);
+        if (pathItem.post) this.addOperationToFolder(path, "POST", pathItem.post, folderName, paramsForPathItem);
+        if (pathItem.delete) this.addOperationToFolder(path, "DELETE", pathItem.delete, folderName, paramsForPathItem);
+        if (pathItem.options) this.addOperationToFolder(path, "OPTIONS", pathItem.options, folderName, paramsForPathItem);
+        if (pathItem.head) this.addOperationToFolder(path, "HEAD", pathItem.head, folderName, paramsForPathItem);
+        if (pathItem.path) this.addOperationToFolder(path, "PATH", pathItem.path, folderName, paramsForPathItem);
+    },
 
-	handlePaths: function(json) {
-		var paths = json.paths;
+    handlePaths: function (json) {
+        var paths = json.paths;
 
-		//Add a folder for each path
-		for(var path in paths){
-		  if (paths.hasOwnProperty(path)) { 
-		    var folderName = this.getFolderNameForPath(path);
-		    if(!folderName) {
-		    	continue;
-		    }
-		    this.logger("Adding path item. path = " + path+"   folder = " + folderName);
-		    this.addPathItemToFolder(path, paths[path], folderName);
-		  }
-		}
-	},
+        //Add a folder for each path
+        for (var path in paths) {
+            if (paths.hasOwnProperty(path)) {
+                var folderName = this.getFolderNameForPath(path);
+                if (!folderName) {
+                    continue;
+                }
+                this.logger("Adding path item. path = " + path + "   folder = " + folderName);
+                this.addPathItemToFolder(path, paths[path], folderName);
+            }
+        }
+    },
 
-	handleParams: function(params, level) {
-		if(!params) return;
-		if(level === "collection") {
-			//base params
-			for(var param in params) {
-				if(params.hasOwnProperty(param)) {
-					this.logger("Adding collection param: " + param);
-					this.basePath[param] = params[param];
-				}
-			}
-			return;
-		}
-	},
+    handleParams: function (params, level) {
+        if (!params) return;
+        if (level === "collection") {
+            //base params
+            for (var param in params) {
+                if (params.hasOwnProperty(param)) {
+                    this.logger("Adding collection param: " + param);
+                    this.basePath[param] = params[param];
+                }
+            }
+            return;
+        }
+    },
 
-	addFoldersToCollection: function() {
-		for(folderName in this.folders) {
-			if(this.folders.hasOwnProperty(folderName)) {
-				this.collectionJson.folders.push(this.folders[folderName]);
-			}
-		}
-	},
+    addFoldersToCollection: function () {
+        for (folderName in this.folders) {
+            if (this.folders.hasOwnProperty(folderName)) {
+                this.collectionJson.folders.push(this.folders[folderName]);
+            }
+        }
+    },
 
-	convert: function(json) {
-		var validationResult = this.validate(json);
-		if(validationResult.status === "failed") {
-			//error
-			return validationResult;
-		}
+    convert: function (json) {
+        var validationResult = this.validate(json);
+        if (validationResult.status === "failed") {
+            //error
+            return validationResult;
+        }
 
-		this.collectionId = uuid.v4();
+        this.collectionId = uuid.v4();
 
-		this.handleParams(json.parameters, "collection")
+        this.handleParams(json.parameters, "collection")
 
-		this.handleInfo(json);
+        this.handleInfo(json);
 
-		this.setBasePath(json);
+        this.setBasePath(json);
 
-		this.handlePaths(json);
+        this.handlePaths(json);
 
-		this.addFoldersToCollection();
-	
-		this.collectionJson.id = this.collectionId;
-		//this.logger(JSON.stringify(this.collectionJson));
-		this.logger("Swagger converted successfully");
-		
-		validationResult.collection = this.collectionJson;
-		
-		return validationResult;
-	},
+        this.addFoldersToCollection();
+
+        this.collectionJson.id = this.collectionId;
+        //this.logger(JSON.stringify(this.collectionJson));
+        this.logger("Swagger converted successfully");
+
+        validationResult.collection = this.collectionJson;
+
+        return validationResult;
+    },
 });
 
 module.exports = Swagger2Postman;
